2562197dd533eb8e094635f20977045121a137da
516108736 <516108736@qq.com>
2019-12-26 11:39:15 +0800

    1139

--- mainnet/checkdb/check_db.sh
+++ mainnet/checkdb/check_db.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-
-
-step=60*60*24  # 24h
 touch /tmp/status.txt
-for (( i = 0; i < 1000000; i=(i+1) )); do
 	git pull
 	go build
 	chmod +x ./stop.sh && ./stop.h
+	NOW=`date +%Y-%m-%d.%H:%M:%S`
 	DATE=`curl https://qkcmainnet-go.s3.amazonaws.com/data/LATEST` >> /tmp/status.txt
+	echo "current time" $NOW  "remote time" $DATE
 	curl https://qkcmainnet-go.s3.amazonaws.com/data/`curl https://qkcmainnet-go.s3.amazonaws.com/data/LATEST`.tar.gz --output data.tar.gz
 	tar xvfz data.tar.gz
 	rm -rf qkc-data && mkdir -p ./qkc-data/
@@ -20,8 +18,9 @@ for (( i = 0; i < 1000000; i=(i+1) )); do
 	./cluster --cluster_config ../../mainnet/singularity/cluster_config_template.json --service S2>> S2.log 2>&1 &
 	./cluster --cluster_config ../../mainnet/singularity/cluster_config_template.json --service S3>> S3.log 2>&1 &
 	sleep 3
-	./cluster --cluster_config ../../mainnet/singularity/cluster_config_template.json  --check_db
-	echo $DATE >> /tmp/status.txt
-
-	sleep $step
-done
+	./cluster --cluster_config ../../mainnet/singularity/cluster_config_template.json  --check_db check_db_rblock_from=1000
+	if [ "${NOW:0:15}" = "${DATE:0:15}" ];then
+		echo "=" >> /tmp/status.txt
+	else
+		echo "!=" >> /tmp/status.txt
+	fi
